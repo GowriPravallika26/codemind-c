f93acd6eb9148ad203666c1ad22994831c5f8199
GowriPravallika26 <147472849+GowriPravallika26@users.noreply.github.com>
2024-03-07 21:17:04 +0530

    #include <stdio.h> int main() {     int n;     scanf("%d",&n);     int arr[n];     for(int i = 0; i < n; i++){         scanf("%d",&arr[i]);     }     int even = 0;     for(int i = 0; i < n; i++){         if(arr[i] % 2 == 0){             even = i;         }     }     printf("%d",even); }

---
+++ Last_even_element_index.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        scanf("%d",&arr[i]);
+    }
+    int even = 0;
+    for(int i = 0; i < n; i++){
+        if(arr[i] % 2 == 0){
+            even = i;
+        }
+    }
+    printf("%d",even);
+}
