9aaf3c67c2c90e19bbadfa2b742c85712f314acc
GowriPravallika26 <147472849+GowriPravallika26@users.noreply.github.com>
2024-03-15 10:39:14 +0530

    #include <stdio.h> int main() {     int m,n;     scanf("%d %d",&m,&n);     int arr[m][n];     for(int i = 0; i < m; i++){         for(int j = 0; j < n; j++){             scanf("%d",&arr[i][j]);         }     }     for(int i = 0; i < m; i++){         int sum = 0;         for(int j = 0; j < n; j++){             sum = sum + arr[i][j];         }         printf("%d ",sum);     } }

---
+++ Row_wise_sum_of_a_matrix.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int m,n;
+    scanf("%d %d",&m,&n);
+    int arr[m][n];
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    for(int i = 0; i < m; i++){
+        int sum = 0;
+        for(int j = 0; j < n; j++){
+            sum = sum + arr[i][j];
+        }
+        printf("%d ",sum);
+    }
+}
